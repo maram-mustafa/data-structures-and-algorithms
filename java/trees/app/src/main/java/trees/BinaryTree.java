34a9364880ae7b52a6507b8d7b453f4959a16517
maram-mustafa <maram.w.mustafa@gmail.com>
2021-08-18 03:50:56 +0300

    not finish

--- java/trees/app/src/main/java/trees/BinaryTree.java
+++ java/trees/app/src/main/java/trees/BinaryTree.java
@@ -65,48 +65,81 @@ public class BinaryTree {
 
   ///////////////////////////////////////// code challenge 16////////////////////////
 
-  public int maximum(Node root) {
-
-//Check whether tree is empty
-    if (root == null) {
-      System.out.println("Tree is empty");
-      return 0;
-    } else {
-      int leftMax;
-      int rightMax;
-      int max = root.value;
-
-      if (root.left != null) {
-        leftMax = maximum(root.left);
-        max = Math.max(max, leftMax);
-      }
-      if (root.right != null) {
-        rightMax = maximum(root.right);
-        max = Math.max(max, rightMax);
-      }
-      return max;
-    }
-  }
+//  public int maximum(Node root) {
+//
+////Check whether tree is empty
+//    if (root == null) {
+//      System.out.println("Tree is empty");
+//      return 0;
+//    } else {
+//      int leftMax;
+//      int rightMax;
+//      int max = root.value;
+//
+//      if (root.left != null) {
+//        leftMax = maximum(root.left);
+//        max = Math.max(max, leftMax);
+//      }
+//      if (root.right != null) {
+//        rightMax = maximum(root.right);
+//        max = Math.max(max, rightMax);
+//      }
+//      return max;
+//    }
+//  }
 
   ///////////////////////////////////////////////// code  challenge 17 ////////////////////////////////////////////
 
-  public List<Integer> breadthFirst(BinarySearchTree tree) throws Exception {
+//  public List<Integer> breadthFirst(BinarySearchTree tree) throws Exception {
+//
+//    Queue breadth = new Queue();
+//    if (root != null) {
+//      breadth.enqueue(root);
+//    }
+//    while (!breadth.isEmpty()) {
+//      Node front = breadth.dequeue();
+//      trees.add(front.value);
+//
+//      if (front.left != null) {
+//        breadth.enqueue(front.left);
+//        if (front.right != null) {
+//          breadth.enqueue(front.right);
+//        }
+//      }
+//    }
+//    return trees;
+//  }
 
-    Queue breadth = new Queue();
-    if (root != null) {
-      breadth.enqueue(root);
-    }
-    while (!breadth.isEmpty()) {
-      Node front = breadth.dequeue();
-      trees.add(front.value);
+  ///////////////////////////////////////////// code challenge 18 ////////////////////////////////////////////////////
 
-      if (front.left != null) {
-        breadth.enqueue(front.left);
-        if (front.right != null) {
-          breadth.enqueue(front.right);
-        }
-      }
+  public static List<String> kTree = new ArrayList<>();
+  public static List<String> FizzBuzz(KTreeNode node) {
+//    if (node == null) {
+//      return kTree;
+//    }
+
+    // Total children count
+    int total = node.children.length;
+    // All the children except the last
+    for (int i = 0; i < total - 1; i++)
+      FizzBuzz(node.children[i]);
+
+    if(node.data %3 == 0 && node.data %5 == 0)
+      kTree.add("FizzBuzz");
+    else if(node.data %3 == 0)
+      kTree.add("Fizz");
+    else if(node.data %5 == 0)
+      kTree.add("Buzz");
+    else{
+      String str = Integer.toString(node.data);
+      kTree.add(str);
     }
-    return trees;
+    // Last child
+    FizzBuzz(node.children[total - 1]);
+
+    return kTree;
   }
+
+
+
 }
---
+++ java/trees/app/src/main/java/trees/KTreeNode.java
@@ -0,0 +1,14 @@
+package trees;
+
+public class KTreeNode{
+  int data;
+  KTreeNode children[];
+
+  public KTreeNode() {
+  }
+
+  KTreeNode(int k, int data) {
+    children = new KTreeNode[k];
+    this.data = data;
+  }
+}
