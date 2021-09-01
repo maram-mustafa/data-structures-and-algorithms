27ed99648061d9115af0c687ba7a9d8ed198cfcd
maram-mustafa <maram.w.mustafa@gmail.com>
2021-09-02 02:37:24 +0300

    solve quick sort

--- java/insertion/app/src/main/java/insertion/App.java
+++ java/insertion/app/src/main/java/insertion/App.java
@@ -42,10 +42,10 @@ public class App {
         j++;
       }
     }
-    while (i < left.length){
+    while (i < left.length) {
       arr[k++] = left[i++];
     }
-    while (j< right.length){
+    while (j < right.length) {
       arr[k++] = right[j++];
     }
   }
@@ -57,10 +57,10 @@ public class App {
       int[] l = new int[mid];
       int[] r = new int[n - mid];
 
-      for (int i=0 ; i<mid;i++){
+      for (int i = 0; i < mid; i++) {
         l[i] = arr[i];
       }
-      for(int i = mid ; i<n;i++){
+      for (int i = mid; i < n; i++) {
         r[i - mid] = arr[i];
       }
       mergeSort(l);
@@ -69,7 +69,48 @@ public class App {
     }
     return arr;
   }
+///////////////////////////////////////////////////////////////code challenge 28 /////////////////////////////////////
+private static int partition(int[] arr, int low, int high) {
+  int pivot = arr[high];
+  int i = (low - 1);
 
+  for (int j = low; j <= high-1; j++) {
+    if (arr[j] <= pivot) {
+      i++;
+
+      swap(arr , i , j);
+
+//      int swap = arr[i];
+//      arr[i] = arr[j];
+//      arr[j] = swap;
+    }
+  }
+  swap(arr,i+1,high);
+  return (i+1);
+
+//  int swap = arr[i + 1];
+//  arr[i + 1] = arr[high];
+//  arr[high] = swap;
+//
+//  return i + 1;
+}
+
+  public static int[] quickSort(int[] arr, int low, int high) {
+    if (low < high) {
+      int p = partition(arr, low, high);
+
+      quickSort(arr, low, p - 1);
+      quickSort(arr, p + 1, high);
+    }
+    return arr;
+  }
+
+  public static void swap(int[] arr , int i , int j){
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j]=temp;
+
+  }
 
   public static void main(String[] args) {
     System.out.println(new App().getGreeting());
@@ -88,29 +129,14 @@ public class App {
 //    //Nearly-sorted
 //    int[] arr3 = {2, 3, 5, 7, 13, 11};
 //    System.out.println(Arrays.toString(insertionSort(arr3)));
-//
-/////////////////////////////////////////////////////////// code challenge 27
-    int[] arr = {8, 4, 23, 42, 16, 15};
-    System.out.println(Arrays.toString(mergeSort(arr)));
+/////////////////////////////////////////////////////////// code challenge 27 /////////////////////////////////////////
+//    int[] array = {8, 4, 23, 42, 16, 15};
+//    System.out.println(Arrays.toString(mergeSort(array)));
+///////////////////////////////////////////////////////////code challenge 28 //////////////////////////////////////////
+    int[] Array = {8,4,23,42,16,15};
+    System.out.println(Arrays.toString(quickSort(Array,0,5)));
+
 
   }
 
-  public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
-
-    int[] arr = {8, 4, 23, 42, 16, 15};
-    System.out.println(Arrays.toString(insertionSort(arr)));
-
-    //Reverse-sorted
-    int[] arr1 = {20,18,12,8,5,-2};
-    System.out.println(Arrays.toString(insertionSort(arr1)));
-
-    //few uniques
-    int[] arr2 = {5,12,7,5,5,7};
-    System.out.println(Arrays.toString(insertionSort(arr2)));
-
-    //Nearly-sorted
-    int[] arr3 = {2,3,5,7,13,11};
-    System.out.println(Arrays.toString(insertionSort(arr3)));
-  }
 }
