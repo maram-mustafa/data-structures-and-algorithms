5a6689efedb70132de8bc4cc9be7c40ca8df455a
maram-mustafa <maram.w.mustafa@gmail.com>
2021-09-05 23:34:22 +0300

    code challenge 31

--- java/HashTable/app/src/test/java/HashTable/AppTest.java
+++ java/HashTable/app/src/test/java/HashTable/AppTest.java
@@ -11,4 +11,18 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+
+    @Test
+  public void testHashTable(){
+      HashTable<String> hash = new HashTable<String>(10);
+      hash.add(10, "maram");
+      hash.add(5, "rand");
+      hash.add(15, "zaina");
+//      System.out.println(hash.get(5));
+//      System.out.println(hash.contains(10));
+
+      assertEquals("rand" , hash.get(5));
+      assertTrue(hash.contains(10));
+
+    }
 }
