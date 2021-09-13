06c18061c8b0bf885903ce87350950417dfa51cf
maram-mustafa <maram.w.mustafa@gmail.com>
2021-09-13 04:40:43 +0300

    not finish

---
+++ java/graphs/app/src/main/java/graphs/Node.java
@@ -0,0 +1,31 @@
+package graphs;
+
+import java.util.Objects;
+
+public class Node{
+
+  String label;
+  Node(String label) {
+    this.label = label;
+  }
+
+  @Override
+  public String toString() {
+    return "Node{" +
+      "label='" + label + '\'' +
+      '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Node node = (Node) o;
+    return Objects.equals(label, node.label);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(label);
+  }
+}
